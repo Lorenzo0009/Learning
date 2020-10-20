8a75d0194dc0b2f2ba15ed86ce58324171f42870
Lorenzo <matheusfertunani@gmail.com>
2020-10-20 16:20:53 -0300

    Initial commit

---
+++ Guanabara Algoritmos/algoritmos.cs
@@ -0,0 +1,17 @@
+/** Algoritmos são passos a serem seguidos por um 
+módulo processador e seus respectivos usuários que, quando executados 
+na ordem correta conseguem realizar determinada tarefa. */
+
+/**Identificadores
+1- Deve começar com uma letra
+2- Os próximos podem ser letras ou números
+3- Não pode utilizar nenhum símbolso, exceto [ _ ]
+4- Não pode conter espaços em branco
+5- Não pode conter letras com acentos
+6- Não pode conter nenhuma palavra reserva (variavel ou nome de métodos)
+
+[Nota1] é uma varíavel ✅
+[Média] não é uma varíavel ❌ (pois feri alguma das regras de criação de variaveis)
+
+
+*/
